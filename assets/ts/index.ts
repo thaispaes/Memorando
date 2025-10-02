b43f8016f17898406db44146ba56c75fda019e6d
Thais Paes <thaisreginapaesdossantos@gmail.com>
2025-10-02 20:52:25 -0300

    Feat: criado logica de aleatorizar images

---
+++ assets/ts/index.ts
@@ -0,0 +1,8 @@
+import { IMAGES, getRandomNumber, selectRandomImages, selectedImages } from './imagens.js';
+
+let dificultyLevel: { [key: string]: number } = {
+    easy: 6,
+    medium: 8,
+    hard: 12,
+};
+
