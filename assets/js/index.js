42b6e86c7b71317e35ed445ef87b4bdac9ae42a2
Thais Paes <thaisreginapaesdossantos@gmail.com>
2025-10-02 22:40:46 -0300

    Feat: suffled indices para aleatorizar mais uma vez a posicao das images

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,30 +1,27 @@
 import { selectRandomImages } from "./imagens.js";
 
-const cardGameIcons = Array.from(document.getElementsByClassName('card-game-icon'));
-
-// cardGameIcons.forEach(card => {
-//   card.addEventListener("click", () => randomImages(dificultyLevel.medium));
-// });
-
 // Definindo níveis de dificuldade
 export let dificultyLevel = {
-    easy: 6,
-    medium: 8,
-    hard: 12,
+  easy: 6,
+  medium: 8,
+  hard: 12,
 };
 
-export function randomImages(dificultyLevel) {
-    const imagesContent = selectRandomImages(dificultyLevel);
-
-    console.log(`imagesContent: ${imagesContent.map(img => img.src)}`);
-
-    const gameImageCard = document.getElementsByClassName('card-game-icon');
-
-    for (let i = 0; i < gameImageCard.length; i++) {
-        gameImageCard[i].src = imagesContent[i].src;
-        console.log(`gameImageCard[${i}]: ${gameImageCard[i]}`);
+function suffleImages(images) {
+    const shuffled = [...images];
+    for (let i = shuffled.length - 1; i > 0; i--) {
+        const randomNumber = Math.floor(Math.random() * (shuffled.length));
+        [shuffled[i], shuffled[randomNumber]] = [shuffled[randomNumber], shuffled[i]];
     }
+    return shuffled;
+}
 
-    
+export function randomImages(dificultyLevel) {
+  const imagesContent = selectRandomImages(dificultyLevel);
+  const gameImageCard = document.getElementsByClassName("card-game-icon");
+  const suffledImages = suffleImages(imagesContent);
 
+  for (let i = 0; i < gameImageCard.length; i++) {
+    gameImageCard[i].src = suffledImages[i].src;
+  }
 }
